149cbc796d4d3a6e2a33ef1bbd5783d556bcfe20
Ângelo Gabriel <angelogabriel207@outlook.com>
2023-02-02 15:21:29 -0300

    AJuste nas configurações iniciais e criação de conexão com Database

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const app = require('express')()
 const consign = require('consign')
+const bodyParser = require('body-parser')
+const routes = require('./config/routes')
+require('./database')
 
-consign()
-    .then('/src/config/middlewares.js')
-    .into(app)
+
+app.use(bodyParser.json())
+app.use(routes)
+
 
 app.listen(4000, () => {
     console.log("Back-end Executando na porta 4000");
